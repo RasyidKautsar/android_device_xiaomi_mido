dcd9fba68ffdc99221fdc8e5b7bb3d58a56b8535
Rsyd (RasyidKautsar) <Rasyid2007AlKautsar@outlook.co.id>
2022-11-12 08:49:43 +0700

    Update SHRP Flags
    Signed-off-by: Rsyd (RasyidKautsar) <Rasyid2007AlKautsar@outlook.co.id>

--- twrp_mido.mk
+++ twrp_mido.mk
@@ -13,6 +13,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# SHRP Stuff
+SHRP_DEVICE_CODE := mido
+SHRP_PATH := device/Xiaomi/$(SHRP_DEVICE_CODE)
+SHRP_MAINTAINER := Rsyd 
+SHRP_REC_TYPE := Normal
+SHRP_DEVICE_TYPE := A-Only
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+SHRP_EDL_MODE := 1
+SHRP_FLASH := 1
+SHRP_EXPRESS := true
+SHRP_EXPRESS_USE_DATA := true
+SHRP_DARK := true
+SHRP_FLASH_MAX_BRIGHTNESS := 255
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
 PRODUCT_NAME := twrp_mido
