c4bdc36929b913246dd536e92414cec8ecc58329
Rsyd (RasyidKautsar) <Rasyid2007AlKautsar@outlook.co.id>
2022-11-12 14:25:43 +0700

    Ckkk
    Signed-off-by: Rsyd (RasyidKautsar) <Rasyid2007AlKautsar@outlook.co.id>

--- omni_mido.mk
+++ omni_mido.mk
@@ -11,25 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
-# SHRP Stuff
-SHRP_DEVICE_CODE := mido
-SHRP_PATH := device/Xiaomi/$(SHRP_DEVICE_CODE)
-SHRP_MAINTAINER := Rsyd 
-SHRP_REC_TYPE := Normal
-SHRP_DEVICE_TYPE := A-Only
-SHRP_REC := /dev/block/bootdevice/by-name/recovery
-SHRP_EDL_MODE := 1
-SHRP_FLASH := 1
-SHRP_EXPRESS := true
-SHRP_EXPRESS_USE_DATA := true
-SHRP_DARK := true
-SHRP_FLASH_MAX_BRIGHTNESS := 255
+# PBRP Stuff
+MAINTAINER := "Rsyd @RasyidKautsar"
+PB_DISABLE_DEFAULT_DM_VERITY := true
+PB_DISABLE_DEFAULT_TREBLE_COMP := true
+PB_TORCH_PATH := /sys/class/leds/led:torch_0
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := twrp_mido
+PRODUCT_NAME := omni_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
